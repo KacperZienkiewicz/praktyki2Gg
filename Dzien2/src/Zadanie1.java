81c4f328a004126993ff2bfc7bd4de9f9dfe0957
KacperZienkiewicz <kacperzienkiewicz1@gmail.com>
2021-02-24 21:21:09 +0100

    Dzien2

---
+++ Dzien2/src/Zadanie1.java
@@ -0,0 +1,12 @@
+public class Zadanie1 {
+    public static void main(String[] args) {
+    char Letter = 'A';
+    for (int i = 0; i < 26; i++)
+    {
+        if ( i % 2 == 0)
+            System.out.print(Letter + " ");
+        Letter++;
+    }
+}
+}
+
