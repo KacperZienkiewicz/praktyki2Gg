fc30be1a8194dd2e3c6feccf62ea1864fdd8a2da
KacperZienkiewicz <kacperzienkiewicz1@gmail.com>
2021-03-09 18:35:11 +0100

    wyjątki

---
+++ wyjątki/zad2/Main.java
@@ -0,0 +1,23 @@
+package wyjątki.zad2;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        try {
+            int[] tablica = new int[4];
+            Scanner scanner = new Scanner(System.in);
+            for (int i = 0; i < 5; i++) {
+                tablica[i] = scanner.nextInt();
+                System.out.println("tablica[" + i + "] ma wartość " + tablica[i]);
+            }
+        }
+        catch (InputMismatchException inputMismatchException){
+            System.out.println("złe liczby :(");
+        }
+        catch (ArrayIndexOutOfBoundsException arrayIndexOutOfBoundsException){
+            System.out.println("przekroczyłeś wielkość tablicy ;/");
+        }
+        }
+
+}
